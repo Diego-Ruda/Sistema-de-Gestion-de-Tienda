fbcea1f31b07b4ad450d3225c25264e02030bb4b
Diego Ruda <diegorudaamd18@gmail.com>
2026-01-31 18:53:31 -0300

    fix: use Railway PG variables for TypeORM

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -3,12 +3,14 @@ import { join } from 'path';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || '3810',
-  database: process.env.DB_NAME || 'fastfood',
+
+  host: process.env.PGHOST,
+  port: Number(process.env.PGPORT),
+  username: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+
   autoLoadEntities: true,
-  synchronize: true,
+  synchronize: false,
   migrations: [join(__dirname, 'migrations/*{.ts,.js}')],
 };
