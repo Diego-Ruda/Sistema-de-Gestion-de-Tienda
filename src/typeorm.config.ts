5399c29d4a21b8f396ebfae5e78db78a607d3444
Diego Ruda <diegorudaamd18@gmail.com>
2026-01-31 19:33:32 -0300

    fix: postgres connection railway

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -1,5 +1,9 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { join } from 'path';
+console.log({
+  PGHOST: process.env.PGHOST,
+  PGPORT: process.env.PGPORT,
+});
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
@@ -8,6 +12,9 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   username: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   autoLoadEntities: true,
   synchronize: false,
   migrations: [join(__dirname, 'migrations/*{.ts,.js}')],
