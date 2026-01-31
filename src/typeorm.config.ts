e0a96148575e0f77f3cb3a9b2d5be338a0ea457c
Diego Ruda <diegorudaamd18@gmail.com>
2026-01-31 19:25:57 -0300

    fix: use railway postgres env vars

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -3,11 +3,11 @@ import { join } from 'path';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.PGHOST,
+  port: Number(process.env.PGPORT),
+  username: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
   autoLoadEntities: true,
   synchronize: false,
   migrations: [join(__dirname, 'migrations/*{.ts,.js}')],
